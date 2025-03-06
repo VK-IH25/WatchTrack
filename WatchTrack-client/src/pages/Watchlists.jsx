fc15eaf22e9c7937a0017815aab2b6f911a4a99c
Victor Abussafi <victorabussafi@gmail.com>
2025-03-06 11:27:59 +0000

    fix no user watchlists

--- WatchTrack-client/src/pages/Watchlists.jsx
+++ WatchTrack-client/src/pages/Watchlists.jsx
@@ -6,9 +6,7 @@ import { AuthContext } from "../context/auth.context";
 
 function Watchlists() {
     const [watchlists, setWatchlists] = useState([]);
-      const { user } = useContext(AuthContext);
-
-
+    const { user } = useContext(AuthContext);
 
     useEffect(() => {
         fetch("http://localhost:5005/watchlist")
@@ -19,17 +17,16 @@ function Watchlists() {
             .catch((err) => console.log(err));
     }, []);
 
-    const filteredWatchlists = watchlists.filter((watchlist) => watchlist.createdBy === user._id);
-    console.log(watchlists);
-
-    console.log(user)
+    let filteredWatchlists = [];
+    
+    if (user) {
+        filteredWatchlists = watchlists.filter((watchlist) => watchlist.createdBy === user._id);
+    }
 
     return (
-      
-            <Container size="xl" mt={80} style={{ padding: "20px 0" }}>
-
+        <Container size="xl" mt={80} style={{ padding: "20px 0" }}>
             <Link to="/watchlists/add">
-            <Button mb={50}>Create Watchlist</Button>
+                <Button mb={50}>Create Watchlist</Button>
             </Link>
 
             <Text size="xl" weight={700} mb="sm">
@@ -47,36 +44,36 @@ function Watchlists() {
                 {watchlists.map((watchlist, index) => (
                     <Carousel.Slide key={index}>
                         <Link to={`/watchlist/${watchlist._id}`}>
-                        <Card shadow="sm" padding="lg">
-                            <Card.Section>
-                                {watchlist.poster_path ? (
-                                    <Image
-                                        src={`https://image.tmdb.org/t/p/w500/${watchlist.poster_path}`}
-                                        height={270}
-                                        fit="cover"
-                                        alt={watchlist.title}
-                                    />
-                                ) : (
-                                    <div
-                                        style={{
-                                            height: 270,
-                                            backgroundColor: "#ccc",
-                                            display: "flex",
-                                            alignItems: "center",
-                                            justifyContent: "center",
-                                        }}
-                                    >
-                                        No Image
-                                    </div>
-                                )}
-                            </Card.Section>
-                            <Text align="center" mt="sm">
-                                {watchlist.title}
-                            </Text>
-                            <Text align="center" mt="sm"> 
-                                {watchlist.description || "No description available"}
-                            </Text>
-                        </Card>
+                            <Card shadow="sm" padding="lg">
+                                <Card.Section>
+                                    {watchlist.poster_path ? (
+                                        <Image
+                                            src={`https://image.tmdb.org/t/p/w500/${watchlist.poster_path}`}
+                                            height={270}
+                                            fit="cover"
+                                            alt={watchlist.title}
+                                        />
+                                    ) : (
+                                        <div
+                                            style={{
+                                                height: 270,
+                                                backgroundColor: "#ccc",
+                                                display: "flex",
+                                                alignItems: "center",
+                                                justifyContent: "center",
+                                            }}
+                                        >
+                                            No Image
+                                        </div>
+                                    )}
+                                </Card.Section>
+                                <Text align="center" mt="sm">
+                                    {watchlist.title}
+                                </Text>
+                                <Text align="center" mt="sm"> 
+                                    {watchlist.description || "No description available"}
+                                </Text>
+                            </Card>
                         </Link>
                     </Carousel.Slide>
                 ))}
@@ -97,43 +94,41 @@ function Watchlists() {
                 {filteredWatchlists.map((watchlist, index) => (
                     <Carousel.Slide key={index}>
                         <Link to={`/watchlist/${watchlist._id}`}>
-                        <Card shadow="sm" padding="lg">
-                            <Card.Section>
-                                {watchlist.poster_path ? (
-                                    <Image
-                                        src={`https://image.tmdb.org/t/p/w500/${watchlist.poster_path}`}
-                                        height={270}
-                                        fit="cover"
-                                        alt={watchlist.title}
-                                    />
-                                ) : (
-                                    <div
-                                        style={{
-                                            height: 270,
-                                            backgroundColor: "#ccc",
-                                            display: "flex",
-                                            alignItems: "center",
-                                            justifyContent: "center",
-                                        }}
-                                    >
-                                        No Image
-                                    </div>
-                                )}
-                            </Card.Section>
-                            <Text align="center" mt="sm">
-                                {watchlist.title}
-                            </Text>
-                            <Text align="center" mt="sm"> 
-                                {watchlist.description || "No description available"}
-                            </Text>
-                        </Card>
+                            <Card shadow="sm" padding="lg">
+                                <Card.Section>
+                                    {watchlist.poster_path ? (
+                                        <Image
+                                            src={`https://image.tmdb.org/t/p/w500/${watchlist.poster_path}`}
+                                            height={270}
+                                            fit="cover"
+                                            alt={watchlist.title}
+                                        />
+                                    ) : (
+                                        <div
+                                            style={{
+                                                height: 270,
+                                                backgroundColor: "#ccc",
+                                                display: "flex",
+                                                alignItems: "center",
+                                                justifyContent: "center",
+                                            }}
+                                        >
+                                            No Image
+                                        </div>
+                                    )}
+                                </Card.Section>
+                                <Text align="center" mt="sm">
+                                    {watchlist.title}
+                                </Text>
+                                <Text align="center" mt="sm"> 
+                                    {watchlist.description || "No description available"}
+                                </Text>
+                            </Card>
                         </Link>
                     </Carousel.Slide>
                 ))}
             </Carousel>
-     
         </Container>
-    
     );
 }
 
