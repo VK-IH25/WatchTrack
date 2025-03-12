e22915e5d327b661222b6702c1d24fd4919dfd2a
Victor Abussafi <victorabussafi@gmail.com>
2025-03-12 10:46:59 +0000

    bug fix netlify

--- WatchTrack-client/src/main.jsx
+++ WatchTrack-client/src/main.jsx
@@ -1,12 +1,12 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
-import App from "./App.jsx";
 import { MantineProvider } from "@mantine/core";
-import myTheme from "./myTheme";
 import { BrowserRouter } from "react-router-dom";
 import { AuthProviderWrapper } from "./context/auth.context";
+import App from "./App.jsx";
+import myTheme from "./myTheme";
 import '@mantine/notifications/styles.css';
+import "./index.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
